fc8ad04c4018675b474fa8d81e3664cd3ece0b8c
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-05-02 21:55:53 -0400

    started adding code to find roku

---
+++ RedTeam/Helper/FindRoku.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RedTeam.Models;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace RedTeam.Helper
+{
+    public class FindRoku
+    {
+        private static void CallRoku()
+        {
+            Boolean done = false;
+            Boolean exception_thrown = false;
+
+            // configure sending socket
+            Socket sending_socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            // set ssdp address and port
+            IPAddress receivingAddress = IPAddress.Parse("192.168.2.255");
+            IPEndPoint receivingEndpoint = new IPEndPoint(receivingAddress, 1900);
+
+            // write the request message
+            var request = new StringBuilder();
+            request.AppendLine("M-SEARCH * HTTP/1.1");
+            request.AppendLine("HOST: 239.255.255.250:1900");
+            request.AppendLine("MAN: \"ssdp:discover\"");
+            request.AppendLine("ST: roku:ecp");
+            request.AppendLine("");
+            string textToSend = request;
+
+            // encode the request into bytes
+            byte[] send_buffer = Encoding.ASCII.GetBytes(request);
+
+
+
+        }
+        
+        
+        
+        
+        public static IPAddress HearRoku()
+        {
+            CallRoku();
+
+
+        }
+    }
+}
---
+++ RedTeam/Models/Roku.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using RedTeam.Helper;
+
+namespace RedTeam.Models
+{
+    public class Roku
+    {
+        public IPAddress Location { get { return FindRoku.HearRoku(); } }
+
+
+    }
+}
