a7007db1633aa382e343eb7bd1f4efc212490730
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-05-05 13:05:30 -0400

    Stopped working on buttons. Switching to quick links

---
+++ RedTeam/ViewModels/RokuApp.js
@@ -0,0 +1,20 @@
+﻿function RokuViewModel() {
+    // Data
+    var self = this;
+    self.ChosenRemoteButton = ko.observable();
+    self.ChosenQuickListID = ko.observable();
+    var selectedRoku = "10.251.1.162";
+    var value = null;
+
+    // Behaviors
+    self.myKeypress = function (value) {
+        var request = "http://10.251.1.162:8060/keypress/home";
+        //var request = "http://" + selectedRoku + ":8060/keypress/" + value;
+        //xmlhttp.open("POST", request, true);
+        //xmlhttp.send();
+        $.post(request);
+    }
+
+};
+
+ko.applyBindings(new RokuViewModel());
