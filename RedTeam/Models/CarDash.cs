27aaf9837a43c96e52883917468e578887aeb2d1
Jon Reed <mr.is@outlook.com>
2015-05-07 09:45:25 -0400

    Corrected view error
    view is working

---
+++ RedTeam/Models/CarDash.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RedTeam.Models
+{
+    public class CarDash
+    {
+        public int Id { get; set; }
+        public bool HeadLightStatus { get; set; }
+        public bool DoorLockStatus { get; set; }
+        public bool HornStatus { get; set; }
+    }
+}
