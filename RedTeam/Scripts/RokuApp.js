6203e8f35f142ba62edb7b44c2c5f986d15ada6e
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-05-07 11:21:06 -0400

    All Roku remote buttons working
    code is ugly and hardcoded. need to do significant cleanup, but moving
    on to other section of roku remote for now

--- RedTeam/Scripts/RokuApp.js
+++ RedTeam/Scripts/RokuApp.js
@@ -1,3 +1,39 @@
-﻿$("#goHome").click(function () {
+﻿$("#home").click(function () {
     $.post("http://10.251.1.162:8060/keypress/home");
+});
+
+$("#back").click(function () {
+    $.post("http://10.251.1.162:8060/keypress/back");
+});
+
+$("#up").click(function () {
+    $.post("http://10.251.1.162:8060/keypress/up");
+});
+
+$("#left").click(function () {
+    $.post("http://10.251.1.162:8060/keypress/left");
+});
+
+$("#right").click(function () {
+    $.post("http://10.251.1.162:8060/keypress/right");
+});
+
+$("#down").click(function () {
+    $.post("http://10.251.1.162:8060/keypress/down");
+});
+
+$("#select").click(function () {
+    $.post("http://10.251.1.162:8060/keypress/select");
+});
+
+$("#rewind").click(function () {
+    $.post("http://10.251.1.162:8060/keypress/rev");
+});
+
+$("#play").click(function () {
+    $.post("http://10.251.1.162:8060/keypress/play");
+});
+
+$("#forward").click(function () {
+    $.post("http://10.251.1.162:8060/keypress/fwd");
 });
